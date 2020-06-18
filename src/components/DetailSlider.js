5da79a9f60d4cb6dd8c1d4267241c9cd7039ea1f
EvgenyOcean <evgeny.ocean@gmail.com>
2020-06-18 16:08:46 +0300

    responsive carousel

--- src/components/DetailSlider.js
+++ src/components/DetailSlider.js
@@ -6,27 +6,61 @@ import detail3 from '../images/LIfestyles/diving2.jpg';
 import {FaAngleLeft, FaAngleRight} from 'react-icons/fa';
 
 function DetailSlider(props) {
-  let counter = 0;
-  const handleClick = (e) => {
-    let slider = document.querySelector('.slides-container'); 
-    let btn = e.target.closest('button');
+  let counter = -1; 
+  let isTransitionOn = false; 
+
+  let handleClick = (e) => {
+    if (isTransitionOn) return; 
+    isTransitionOn = true; 
+    let slider = document.querySelector('.slides-container');
+    let slides = document.querySelectorAll('.slide');
+    let btn = e.target.closest('button'); 
+
+    const lastMainImg = -(slides.length - 2);  
+    const lastDecorativeImg = -(slides.length - 1) 
+
     if (btn.id === 'left'){
       counter++; 
+      slider.style.transition = 'all .3s';
       slider.style.marginLeft = `${counter * 90}vw`; 
+
     } else if (btn.id === 'right'){
       counter--; 
+      slider.style.transition = 'all .3s';
       slider.style.marginLeft = `${counter * 90}vw`; 
     }
+
+    document.addEventListener('transitionend', function inner(e) {
+      if (counter === 0){ 
+        slider.style.transition = 'none'; 
+        counter = lastMainImg; 
+        slider.style.marginLeft = `${counter * 90}vw`; 
+      } 
+
+      if (counter === lastDecorativeImg){
+        slider.style.transition = 'none';
+        counter = -1; 
+        slider.style.marginLeft = `${counter * 90}vw`; 
+      }
+
+      document.removeEventListener('transitionend', inner);
+      isTransitionOn = false;
+    })
   }
+
 
   return (
     <DetailSliderDIV>
       <button id="left" className="slider-btn slider-btn--left" onClick={handleClick}><FaAngleLeft/></button>
       <div className="slider-container">        
         <div className="slides-container">
-          <img src={detail} alt="" className="slide"></img>
+          <img src={detail3} alt="" className="slide"></img> {/* 0 */}
+
+          <img src={detail} alt="" className="slide"></img> {/* -1 */}
           <img src={detail2} alt="" className="slide"></img>
-          <img src={detail3} alt="" className="slide"></img>
+          <img src={detail3} alt="" className="slide"></img> {/* -3 */}
+
+          <img src={detail} alt="" className="slide"></img> {/* -4 */}
         </div>
       </div>
       <button id="right" className="slider-btn slider-btn--right" onClick={handleClick}><FaAngleRight/></button>
--- src/components/DetailSliderSC.js
+++ src/components/DetailSliderSC.js
@@ -1,24 +1,27 @@
 import styled from 'styled-components';
 
 const DetailSliderDIV = styled.div`
-  width: 100%;
+  width: 100vw;
+  max-width: 1920px;
+  margin: 0 auto;
   padding: 2rem 5vw;
   position: relative;
   
   .slider-container{
     overflow: hidden;
-
   }
 
   .slides-container{
-    width: 9999px;
     display: flex;
     height: 40vh;
     min-height: 570px;
+    transition: all .3s ease-in;
+    margin-left: -90vw;
 
     .slide{
       width: 90vw;
       height: 100%;
+      flex-shrink: 0;
       object-fit: cover; 
     }
   }
@@ -33,6 +36,7 @@ const DetailSliderDIV = styled.div`
     cursor: pointer;
     
     svg{
+      color: var(--darkGrey);
       width: 2rem;
       height: 10rem;
       vertical-align: bottom;
@@ -40,12 +44,35 @@ const DetailSliderDIV = styled.div`
   }
 
   .slider-btn--left{
-    left: 1rem; 
+    left: 1.5vw; 
   }
 
   .slider-btn--right{
-    right: 1rem; 
+    right: 1.5vw; 
   }
+
+  @media (max-width: 1100px){ 
+    .slider-btn--left{
+      left: 0.3vw; 
+    }
+
+    .slider-btn--right{
+      right: 0.3vw; 
+    }
+  }
+
+  @media (max-width: 768px){ 
+    .slider-btn--left{
+      left: 5vw; 
+      color: white;
+    }
+
+    .slider-btn--right{
+      right: 5vw; 
+      color: white;
+    }
+  }
+
 `
 
 export default DetailSliderDIV;
